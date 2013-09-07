556cb95835f4a3e2b318dac1d25f70958347e35f
Szymon Nowak <szimek@gmail.com>
2013-09-07 09:04:38 +0200

    Update js.

--- js/signature_pad.js
+++ js/signature_pad.js
@@ -1,19 +1,23 @@
-// Signature Pad
-// https://github.com/szimek/signature_pad
-//
-// Copyright 2013 Szymon Nowak
-// Released under the MIT license
-//
-// The main idea and some parts of the code (e.g. drawing variable width Bézier curve) are taken from:
-// http://corner.squareup.com/2012/07/smoother-signatures.html
-//
-// Implementation of interpolation using cubic Bézier curves is taken from:
-// http://benknowscode.wordpress.com/2012/09/14/path-interpolation-using-cubic-bezier-and-control-point-estimation-in-javascript
-//
-// Algorithm for approximated length of a Bézier curve is taken from:
-// http://www.lemoda.net/maths/bezier-length/index.html
-//
+/*!
+ * Signature Pad v1.0.0
+ * https://github.com/szimek/signature_pad
+ *
+ * Copyright 2013 Szymon Nowak
+ * Released under the MIT license
+ *
+ * The main idea and some parts of the code (e.g. drawing variable width Bézier curve) are taken from:
+ * http://corner.squareup.com/2012/07/smoother-signatures.html
+ *
+ * Implementation of interpolation using cubic Bézier curves is taken from:
+ * http://benknowscode.wordpress.com/2012/09/14/path-interpolation-using-cubic-bezier-and-control-point-estimation-in-javascript
+ *
+ * Algorithm for approximated length of a Bézier curve is taken from:
+ * http://www.lemoda.net/maths/bezier-length/index.html
+ *
+ */
 var SignaturePad = (function (document) {
+    "use strict";
+
     var SignaturePad = function (canvas, options) {
         var self = this,
             opts = options || {};
