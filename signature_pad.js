436980191a19bc28a8d20ef82b08371b08941be1
Rogerz Zhang <rogerz.zhang@gmail.com>
2013-11-22 19:36:23 +0800

    Fix bug in touchend

--- signature_pad.js
+++ signature_pad.js
@@ -146,8 +146,9 @@ var SignaturePad = (function (document) {
 
         document.addEventListener("touchend", function (event) {
             var wasCanvasTouched = event.target === self._canvas;
+            var touch = event.changeTouches[0];
             if (wasCanvasTouched) {
-                self._strokeEnd();
+                self._strokeEnd(touch);
             }
         });
     };
