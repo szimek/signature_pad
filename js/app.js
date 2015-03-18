e968a413116bc59e4997b552dfa1394bff5164f3
Szymon Nowak <szimek@gmail.com>
2015-03-18 18:13:37 +0100

    Try to fix issue with zooming out. Hopefully it doesn't break anything

--- js/app.js
+++ js/app.js
@@ -8,7 +8,10 @@ var wrapper = document.getElementById("signature-pad"),
 // to make it look crisp on mobile devices.
 // This also causes canvas to be cleared.
 function resizeCanvas() {
-    var ratio =  window.devicePixelRatio || 1;
+    // When zoomed out to less then 100%, for some very strange reason,
+    // some browsers report devicePixelRatio as less than 1
+    // and only part of the canvas is cleared then.
+    var ratio =  Math.max(window.devicePixelRatio || 1, 1);
     canvas.width = canvas.offsetWidth * ratio;
     canvas.height = canvas.offsetHeight * ratio;
     canvas.getContext("2d").scale(ratio, ratio);
