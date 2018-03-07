722179182a4b0f6793005a6f26c82b20637de09e
Szymon Nowak <szimek@gmail.com>
2018-03-07 20:52:50 +0100

    Change Point#velocityFrom to return 0 if times are equal

--- docs/js/signature_pad.umd.js
+++ docs/js/signature_pad.umd.js
@@ -28,14 +28,14 @@ var Point = (function () {
         this.y = y;
         this.time = time || Date.now();
     }
-    Point.prototype.velocityFrom = function (start) {
-        return (this.time !== start.time) ? this.distanceTo(start) / (this.time - start.time) : 1;
-    };
     Point.prototype.distanceTo = function (start) {
         return Math.sqrt(Math.pow(this.x - start.x, 2) + Math.pow(this.y - start.y, 2));
     };
     Point.prototype.equals = function (other) {
         return this.x === other.x && this.y === other.y && this.time === other.time;
+    };
+    Point.prototype.velocityFrom = function (start) {
+        return (this.time !== start.time) ? this.distanceTo(start) / (this.time - start.time) : 0;
     };
     return Point;
 }());
--- src/point.ts
+++ src/point.ts
@@ -12,15 +12,15 @@ export class Point implements IBasicPoint {
     this.time = time || Date.now();
   }
 
-  public velocityFrom(start: IBasicPoint): number {
-    return (this.time !== start.time) ? this.distanceTo(start) / (this.time - start.time) : 1;
-  }
-
   public distanceTo(start: IBasicPoint): number {
     return Math.sqrt(Math.pow(this.x - start.x, 2) + Math.pow(this.y - start.y, 2));
   }
 
   public equals(other: IBasicPoint): boolean {
     return this.x === other.x && this.y === other.y && this.time === other.time;
+  }
+
+  public velocityFrom(start: IBasicPoint): number {
+    return (this.time !== start.time) ? this.distanceTo(start) / (this.time - start.time) : 0;
   }
 }
