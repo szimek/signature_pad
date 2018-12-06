d75ad2c1c350592d31054fa73bcfe67a745b323b
Samuel Priddy <sam@scriptdrop.co>
2018-12-06 13:17:09 -0500

    do not factor viewBox size by devicePixelDensity

--- src/signature_pad.ts
+++ src/signature_pad.ts
@@ -563,7 +563,7 @@ export default class SignaturePad {
       '<svg' +
       ' xmlns="http://www.w3.org/2000/svg"' +
       ' xmlns:xlink="http://www.w3.org/1999/xlink"' +
-      ` viewBox="${minX} ${minY} ${maxX} ${maxY}"` +
+      ` viewBox="${minX} ${minY} ${this.canvas.width} ${this.canvas.height}"` +
       ` width="${maxX}"` +
       ` height="${maxY}"` +
       '>';
