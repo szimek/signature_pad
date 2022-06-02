1a401fd98065570620716f0f68046df0a651c409
Tony Brix <tony@brix.ninja>
2022-06-02 00:29:44 -0500

    chore: modernize demo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
+  env: {
+    browser: true
+  },
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
---
+++ docs/.eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  globals: {
+    SignaturePad: false
+  }
+};
