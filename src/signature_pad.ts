467a7f4e23b7357987f576ade357c32764c2a8b9
Tony Brix <tony@brix.ninja>
2022-11-08 06:14:46 +0000

    fix: undo fix zoom

--- src/signature_pad.ts
+++ src/signature_pad.ts
@@ -437,15 +437,9 @@ export default class SignaturePad extends SignatureEventTarget {
   private _createPoint(x: number, y: number, pressure: number): Point {
     const rect = this.canvas.getBoundingClientRect();
 
-    const dpr = Math.max(window.devicePixelRatio || 1, 1);
-    const zoom =
-      this.canvas.width && rect.width
-        ? this.canvas.width / rect.width / dpr
-        : 1;
-
     return new Point(
-      x - rect.left * zoom,
-      y - rect.top * zoom,
+      x - rect.left,
+      y - rect.top,
       pressure,
       new Date().getTime(),
     );
